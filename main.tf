199f507c41d8230ddda2f87935dcc115afce49fc
Azhari <fathurizqi.b.azhari@accenture.com>
2025-10-23 10:03:56 +0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+  default     = "test-pipeline"
+}
+
+# Create S3 bucket
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = "dev"
+  }
+}
