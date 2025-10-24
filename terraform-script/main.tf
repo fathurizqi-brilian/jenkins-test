56aa081487f1d10314864b372efb95d31a09c7c2
Azhari <fathurizqi.b.azhari@accenture.com>
2025-10-24 10:50:14 +0700

    add rules

--- terraform-script/main.tf
+++ terraform-script/main.tf
@@ -13,17 +13,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-variable "aws_region" {
-  description = "AWS region to deploy resources"
-  type        = string
-  default     = "ap-southeast-1"
-}
-
-variable "bucket_name" {
-  description = "Name of the S3"
-  type        = string
-  default     = "test-pipeline-898203562451-bucket"
-}
 
 # Create S3 bucket
 resource "aws_s3_bucket" "demo_bucket" {
@@ -35,15 +24,15 @@ resource "aws_s3_bucket" "demo_bucket" {
   }
 }
 
-# resource "aws_s3_bucket_lifecycle_configuration" "demo_lifecycle" {
-#   bucket = aws_s3_bucket.demo_bucket.id
+resource "aws_s3_bucket_lifecycle_configuration" "demo_lifecycle" {
+  bucket = aws_s3_bucket.demo_bucket.id
 
-#   rule {
-#     id     = "abort-incomplete-multipart-uploads"
-#     status = "Enabled"
+  rule {
+    id     = "abort-incomplete-multipart-uploads"
+    status = "Enabled"
 
-#     abort_incomplete_multipart_upload {
-#       days_after_initiation = 7  # Delete incomplete parts after 7 days
-#     }
-#   }
-# }
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7  # Delete incomplete parts after 7 days
+    }
+  }
+}
