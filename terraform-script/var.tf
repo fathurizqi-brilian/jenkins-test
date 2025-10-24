c029ae06317a40af8001ed6e7b477771c689be2b
Azhari <fathurizqi.b.azhari@accenture.com>
2025-10-24 10:52:01 +0700

    add lifecycles

---
+++ terraform-script/var.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3"
+  type        = string
+  default     = "test-pipeline-898203562451-bucket"
+}
