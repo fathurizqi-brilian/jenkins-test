ad539c201ecf44b9cd5981ea3bf141ed4023d03b
Azhari <fathurizqi.b.azhari@accenture.com>
2025-10-24 08:46:43 +0700

    add creds

--- vars/TerraformCreateS3.groovy
+++ vars/TerraformCreateS3.groovy
@@ -29,7 +29,14 @@ def call(Map config = [:]) {
 
             stage('Apply') {
                 steps {
-                    dir(tfDir) {
+                    dir(tfDir) 
+                        withCredentials([
+                            aws(
+                                credentialsId: awsCreds,
+                                accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                                secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                            )
+                        ]) {
                         input message: 'Approve apply?'
                         sh """
                             export AWS_DEFAULT_REGION=${region}
